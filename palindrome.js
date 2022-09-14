63ab92f0b5051aa908946313a115dff8d33546c2
Anandhu__P_A <anandhupa.in@gmail.com>
2022-09-14 19:32:53 +0530

    check palindrome added

---
+++ palindrome.js
@@ -0,0 +1,8 @@
+//check palindrome
+var str ="abc";
+var bag ="";
+for(var i=str.length-1;i>=0;i--){
+  bag = bag +str[i];
+}
+if(str==bag){console.log("Not Palindrome")}
+else{console.log("Not Palindrome")}
